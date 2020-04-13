505fa35ffbb92cbbe27abee5f9a5f784ed6bf7c3
tsaristbomba <andrexerdna@gmail.com>
2020-04-13 20:12:48 -0300

    Updates

---
+++ precache-manifest.c949496e379309ac14c03540a9d6aa6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd95446edce7fda0eaae85031bb17153",
+    "url": "/iss-location.git/index.html"
+  },
+  {
+    "revision": "cdf91c468547535894ef",
+    "url": "/iss-location.git/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "cdf91c468547535894ef",
+    "url": "/iss-location.git/static/js/2.62960ae0.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/iss-location.git/static/js/2.62960ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3474ecd3f13529da835b",
+    "url": "/iss-location.git/static/js/main.7200ed74.chunk.js"
+  },
+  {
+    "revision": "bf7f7f16f3a52e8174cb",
+    "url": "/iss-location.git/static/js/runtime-main.3dc9f95c.js"
+  }
+]);
